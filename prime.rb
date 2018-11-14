20887feab817548deaea6d48facd93af69f1d021
Simon Balean <simonbalean@gmail.com>
2018-11-14 07:30:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(number)
+  
+  if number % 2 == 0 
+    FALSE
+    elsif number % 3 == 0 
+    FALSE
+    elsif number % 4 == 0 
+    FALSE
+    elsif number % 5 == 0 
+    FALSE
+    elsif number % 6 == 0 
+    FALSE
+    elsif number % 7 == 0 
+    FALSE
+    elsif number % 8 == 0 
+    FALSE
+    elsif number % 9 == 0 
+    FALSE
+    elsif number % 10 == 0 
+    FALSE
+  else TRUE
+  end
+end
