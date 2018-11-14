1c257ea4fb3f07d4d96de37e0ed6dfc3c06bcf16
Simon Balean <simonbalean@gmail.com>
2018-11-14 07:36:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,15 @@
 # Add  code here!
 def prime?(integer)
+  if integer < 2 
+    return FALSE
+  else (2..integer)
+  
+  
+  
+  
+  
+  
+  
   return FALSE if integer < 2
   (2..integer - 1).each do |number|
     return FALSE if integer % number == 0
